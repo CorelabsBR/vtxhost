d6974205767b52c8d4c91294699acdd2f8c7a8b5
Arcaridev <kelvinarcari@gmail.com>
2026-04-17 16:59:36 -0300

    refasendo o site do zero vamos la tudo do zero meu deus estou ficando louco nao aguento mais
    Signed-off-by: Arcaridev <kelvinarcari@gmail.com>

---
+++ vtxhost/src/main/java/br/com/corelabs/vortexhost/vtxhost/VtxhostApplication.java
@@ -0,0 +1,13 @@
+package br.com.corelabs.vortexhost.vtxhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtxhostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtxhostApplication.class, args);
+	}
+
+}
---
+++ vtxhost/src/test/java/br/com/corelabs/vortexhost/vtxhost/VtxhostApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.corelabs.vortexhost.vtxhost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtxhostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
